6709bbcea3af5be5e986677b6cfaf85277bc5056
Brian Crossley <bcrossley712@gmail.com>
2022-07-17 23:15:01 -0600

    ??

--- src/AppState.js
+++ src/AppState.js
@@ -1,4 +1,4 @@
-import { reactive } from 'vue'
+import { reactive } from '../node_modules/vue/dist/vue.d.ts'
 
 // NOTE AppState is a reactive object to contain app level data
 export const AppState = reactive({
