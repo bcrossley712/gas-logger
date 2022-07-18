f7e1502fe53ef8db707efbd388d0f1e2b39efc04
Brian Crossley <bcrossley712@gmail.com>
2022-07-17 23:06:27 -0600

    i don't know anything

--- src/AppState.js
+++ src/AppState.js
@@ -1,4 +1,4 @@
-import { reactive } from 'vue'
+import { reactive } from '../node_modules/vue/dist/vue'
 
 // NOTE AppState is a reactive object to contain app level data
 export const AppState = reactive({
