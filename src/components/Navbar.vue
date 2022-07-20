1239735b2f8ad889dac3242e1db366abe40d8d31
Brian Crossley <bcrossley712@gmail.com>
2022-07-20 14:08:33 -0600

    update heading

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-3">
     <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
       <div class="d-flex flex-column align-items-center">
-        <h3>Joe gas keeper</h3>
+        <h3>Joe gas logger</h3>
       </div>
     </router-link>
     <button
